1d4b2da011fa97c5040666f88765c44ca5ac3203
danliang <dan.liangd@meviky.com>
2022-07-01 09:11:24 +0800

    增加eslint规范

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+	printWidth: 120, // 每行代码长度（默认80）
+	tabWidth: 4, // 每个tab相当于多少个空格（默认2）
+	useTabs: true, // 是否使用tab进行缩进（默认false）
+	singleQuote: false, // 使用单引号（默认false）[false:不使用单引号]
+	semi: false, // 声明结尾使用分号(默认true)[false:不加分号]
+	trailingComma: "none"
+}
